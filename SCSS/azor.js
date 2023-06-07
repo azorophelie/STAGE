77fd6c0cfc96d4467fc7965c2369882e0b4ee0f3
azorophelie <azor.ophelie@yahoo.fr>
2023-06-07 16:47:32 +0200

    azor07/06

--- SCSS/azor.js
+++ SCSS/azor.js
@@ -4,9 +4,42 @@ function toggleDarkMode() {
     darkModeIcon.textContent = body.classList.contains('dark-mode') ? '☀️' : '🌙';
 
   }
+  function toggleDarkMode() {
+    var body = document.getElementsByTagName("body")[0];
+    body.classList.toggle("dark-mode");
+    dark.textContent = body.classList.contains('dark-mode') ? '☀️' : '🌙';
+  }
   function scrollToTop() {
     window.scrollTo({
       top: 0,
       behavior: "smooth"
     });
   }
+  document.addEventListener('DOMContentLoaded', function() {
+    var dropDownBtn = document.querySelector('.dropbtn');
+    var dropDownContent = document.querySelector('.dropdown-content');
+  
+    dropDownBtn.addEventListener('click', function() {
+      if (dropDownContent.style.display === 'block') {
+        dropDownContent.style.display = 'none';
+      } else {
+        dropDownContent.style.display = 'block';
+      }
+    });
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var menuItems = document.querySelectorAll(".mobile-menu li");
+  
+    menuItems.forEach(function(item) {
+      item.addEventListener("click", function(e) {
+        e.stopPropagation();
+        item.classList.toggle("open");
+      });
+    });
+  
+    document.addEventListener("click", function() {
+      menuItems.forEach(function(item) {
+        item.classList.remove("open");
+      });
+    });
+  });
